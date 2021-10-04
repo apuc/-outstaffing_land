979fcc168473b8640e68810b9ea3a948a0d20889
denkalinovsky <denkalinovsky@gmail.com>
2021-10-04 22:37:48 +0300

    new(config): добавил файл конфигурации
    файл конфигурации для появления изображений

---
+++ constants/config.js
@@ -0,0 +1,10 @@
+export const CONFIG_IMG_SHOW = {
+    reset: true,
+    duration: 1600,
+    distance: '0px',
+    // opacity: 0.8,
+    viewFactor: 0.5,
+    viewOffset: {
+        top: 60
+    }
+}
