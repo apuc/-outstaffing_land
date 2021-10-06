7f58b228ffecff05f91b7eb47be9f5fa9a90aa4f
DenKalinovsky <denkalinovsky@gmail.com>
2021-10-06 13:43:44 +0300

    new(package, App): подключил element-ui

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,11 @@
 
 <script>
 import ItOutstaffing from './view/ItOutstaffing.vue'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(ElementUI);
 export default {
   name: 'App',
   components: {
